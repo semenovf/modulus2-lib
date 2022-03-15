cb66e29e8114cf6f01286f6c093b60576574d8ed
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-03-15 21:35:04 +0500

    fix: Fixed warning message in `rocksdb_settings`

--- include/pfs/modulus2/plugins/rocksdb_settings.hpp
+++ include/pfs/modulus2/plugins/rocksdb_settings.hpp
@@ -69,7 +69,7 @@ protected:
         auto value = _db.fetch(key, & ok);
 
         if (!ok) {
-            this->failure(fmt::format("no property found by key: [{}]", key));
+            this->failure(fmt::format("no property found by key: [{}], used default", key));
             return default_value;
         }
 
