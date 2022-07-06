fdb65eee1604c8e9cb207bfa7524f53db17df616
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-06 15:54:07 +0500

    fix: Updated build scripts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 BASE_URL=https://raw.githubusercontent.com/semenovf/scripts/master
+PORTABLE_TARGET_BASE_URL=https://raw.githubusercontent.com/semenovf/portable-target/master/cmake/v2
+BAK_SUFFIX=`date +%Y_%m_%d_%H_%M_%S`
 
 SCRIPTS="build.sh \
     build-windows.cmd"
 
-CONFIGS="CMakeLists.txt \
-    .gitignore \
+CONFIGS=".gitignore \
     .travis.yml \
     appveyor.yml \
     codecov.yml"
@@ -16,12 +17,17 @@ LAST_COMAMND=
 
 curl_copy()
 {
-    FILENAME=$1
-    SOURCE=$2
-    TARGET=$3
+    SOURCE=$1
+    TARGET=$2
+    FILENAME=`basename $TARGET`
+
+    if [ -e $TARGET ] ; then
+        echo "Target already exists: $TARGET, new one will be renamed into: $TARGET.$BAK_SUFFIX"
+        TARGET="$TARGET.$BAK_SUFFIX"
+    fi
 
     LAST_COMMAND="curl -s $SOURCE -o $TARGET"
-    echo -n "Copying $FILENAME ... "
+    echo -n "Coping $FILENAME ... "
 
     $LAST_COMMAND > /dev/null
     LAST_STATUS=$?
@@ -29,37 +35,46 @@ curl_copy()
     if [ $LAST_STATUS -eq 0 ] ; then
         echo "OK"
     else
-        echo "ERROR: failure on command"
+        echo "Failure on command"
         echo "\t$LAST_COMMAND"
     fi
 }
 
 for f in $SCRIPTS ; do
-    curl_copy $f $BASE_URL/scripts/${f} $f
+    curl_copy $BASE_URL/scripts/${f} $f
     [ $LAST_STATUS -ne 0 ] && exit 1
 done
 
 for f in $CONFIGS ; do
-    curl_copy $f $BASE_URL/configs/${f}  $f
+    curl_copy $BASE_URL/configs/${f} $f
     [ $LAST_STATUS -ne 0 ] && exit 1
 done
 
-echo "\nNow it is time to edit CMakeLists.txt to complete project basic initialization"
-
-if [ -z "${EDITOR}" ] ; then
-    if command -v mcedit > /dev/null ; then
-        EDITOR=mcedit
-    elif command -v nano > /dev/null ; then
-        EDITOR=nano
-    elif command -v vim > /dev/null ; then
-        EDITOR=vim
-    elif command -v vi > /dev/null ; then
-        EDITOR=vi
-    else
-        echo "ERROR: No any known editor found"
-    fi
-fi
-
-if [ -n "${EDITOR}" ] ; then
-    $EDITOR CMakeLists.txt
-fi
+######################################################
+# portable-target
+######################################################
+#mkdir -p cmake/portable-target/actions
+#
+#PORTABLE_TARGET="ColouredMessage.cmake \
+#    Functions.cmake \
+#    PortableTarget.cmake"
+#
+#PORTABLE_TARGET_ACTIONS="add_executable.cmake \
+#    add_library.cmake \
+#    compile_options.cmake \
+#    definitions.cmake \
+#    include_directories.cmake \
+#    link_libraries.cmake \
+#    link_qt5_components.cmake \
+#    properties.cmake \
+#    sources.cmake"
+#
+#for f in $PORTABLE_TARGET ; do
+#    curl_copy $PORTABLE_TARGET_BASE_URL/${f} cmake/portable-target/$f
+#    [ $LAST_STATUS -ne 0 ] && exit 1
+#done
+#
+#for f in $PORTABLE_TARGET_ACTIONS ; do
+#    curl_copy $PORTABLE_TARGET_BASE_URL/actions/${f} cmake/portable-target/actions/$f
+#    [ $LAST_STATUS -ne 0 ] && exit 1
+#done
