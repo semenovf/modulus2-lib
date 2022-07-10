d54f97d4938e0dab2eba21940048192510e991f2
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 20:26:36 +0500

    Updated `update.sh`.

--- update.sh
+++ update.sh
@@ -5,17 +5,17 @@ SPDLOG_RELEASE=v1.10.0
 
 if [ -d .git ] ; then
 
-    git pull \
+    git checkout master && git pull origin master \
         && git submodule update --init \
         && git submodule update --remote \
         && cd 3rdparty/portable-target && git checkout master && git pull \
         && cd $CWD \
-        && cd 3rdparty/pfs/common && git checkout master && git pull \
+        && cd 3rdparty/pfs/common && ./update.sh \
         && cd $CWD \
-        && cd 3rdparty/pfs/debby && git checkout master && git pull \
-            && git submodule update --init -- 3rdparty/rocksdb \
+        && cd 3rdparty/pfs/debby && ./update.sh \
         && cd $CWD \
         && cd 3rdparty/spdlog && git checkout $SPDLOG_RELEASE \
-
+        && cd $CWD
+        
 fi
 
