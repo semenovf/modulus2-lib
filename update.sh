dd7b92556626116287ecb670d4de29991a334db5
Fedor Semenov <fedor.v.semenov@gmail.com>
2022-07-17 15:40:51 +0500

    Fixed build for `MSVC`.

--- update.sh
+++ update.sh
@@ -2,18 +2,16 @@
 
 CWD=`pwd`
 SPDLOG_RELEASE=v1.10.0
+ROCKSDB_RELEASE=v6.29.5
 
 if [ -d .git ] ; then
 
     git checkout master && git pull origin master \
-        && git submodule update --init \
-        && git submodule update --remote \
-        && cd 3rdparty/portable-target && git checkout master && git pull \
-        && cd $CWD \
-        && cd 3rdparty/pfs/common && ./update.sh \
-        && cd $CWD \
-        && cd 3rdparty/pfs/debby && ./update.sh \
-        && cd $CWD \
+        && git submodule update --init --recursive \
+        && git submodule update --init --remote -- 3rdparty/portable-target \
+        && git submodule update --init --remote -- 3rdparty/pfs/common \
+        && git submodule update --init --remote -- 3rdparty/pfs/debby \
+        && cd 3rdparty/pfs/debby/3rdparty/rocksdb && git checkout $ROCKSDB_RELEASE \
         && cd 3rdparty/spdlog && git checkout $SPDLOG_RELEASE \
         && cd $CWD
         
